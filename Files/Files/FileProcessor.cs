a1819fde6d4090d27d16b966ade314b884dc18cc
anurag19289 <53707071+anurag19289@users.noreply.github.com>
2019-08-04 17:57:35 +0100

    Add files via upload

---
+++ Files/Files/FileProcessor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+
+namespace Files
+{
+    internal class FileProcessor
+    {
+        private string filePath;
+
+        public FileProcessor(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        internal void Process()
+        {
+            WriteLine($"File processing started");
+
+            //check if file exists
+            if (!File.Exists(filePath))
+            {
+                WriteLine($"file doesnot exist {filePath}");
+                return;
+            }
+            else
+            {
+                WriteLine($"file exists {filePath}");
+
+            }
+        }
+    }
+}
---
+++ Files/Files/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using static System.Console;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WriteLine("Parsing the command line options");
+
+            var command = args[0];
+
+            if (command == "--file")
+            {
+                var filePath = args[1];
+                WriteLine($"single file {filePath} selected");
+                ProcessSingleFile(filePath);
+            }
+            else if (command == "--dir")
+            {
+                var dirPath = args[1];
+                var fileType = args[2];
+                WriteLine($"Directory  {dirPath} selected for {fileType} files");
+                ProcessDirectory(dirPath, fileType);
+
+            }
+            else
+            {
+                WriteLine("Invalid command line option");
+            }
+            WriteLine("Press enter to exit");
+            ReadLine();
+
+        }
+        private static void ProcessSingleFile(string filePath)
+        {
+            var fileProcessor = new FileProcessor(filePath);
+            fileProcessor.Process();
+        }
+        private static void ProcessDirectory(string dirPath, string fileType)
+        {
+
+        }
+
+
+    }
+}
